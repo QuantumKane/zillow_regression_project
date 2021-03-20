84f2422d618379f38152f775993f36f6e946d1dd
Steve Kane <quantumkane00@gmail.com>
2021-03-19 20:54:24 -0500

    edited SQL query

--- acquire.py
+++ acquire.py
@@ -16,11 +16,12 @@ def get_zillow_data():
     '''
     This function uses the SQL query from below and specifies the database to use
     '''
-    # SQL query that joins all of the tables together from the 'telco_churn' database     
+    # SQL query that joins tables together from the 'zillow' database     
     sql_query = """
                 SELECT * 
                 FROM properties_2017
                 JOIN predictions_2017 USING(parcelid)
-                WHERE transactiondate BETWEEN "2017-05-01" AND "2017-06-30";
+                WHERE transactiondate BETWEEN "2017-05-01" AND "2017-08-31"
+                AND propertylandusetypeid = 261 OR 263 OR 273 OR 279;
                 """
     return pd.read_sql(sql_query,get_connection('zillow'))
